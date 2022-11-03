0c81ce27d2982c7f06ba147bedb6bbc6d4e77ec7
kushal <kushaladh6@gmail.com>
2022-11-03 07:31:32 +0545

    backend to frontend

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,15 +1,29 @@
+import { useEffect,useState } from "react";
+
 const Login =()=>{
+    const [backName,setBackName]=useState('')
+    useEffect(()=>{
+        fetch('http://localhost:3001/me').then((res)=>res.json())
+        .then((data)=>setBackName(data.name))
+    },[])
     return (
         <div>
             <form>
             <label>Name:
-            <input id ="name" type = "text" placeholder = "name" ></input>
+            <input id ="name" type = "text" placeholder = "name"  ></input>
+            </label><br/>
+            <label>Address:
+            <input id="address" type = "text" placeholder = "address"></input>
+            </label><br/>
+            <label>Email-id:
+            <input id="email" type = "mail" placeholder = "email-id"></input>
             </label><br/>
             <label>Password:
             <input id="password" type = "password" placeholder = "password"></input>
             </label><br/>
-            <input type="submit" value="Submit"/>
+            <input type="submit" on value="Submit"/>
         </form> 
+        {backName}
         </div>
     )
 }
